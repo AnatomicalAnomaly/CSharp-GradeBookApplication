da55b22624b4bfb8bb4a673fcc9d7e68d8b7810b
vaughn.devin.1 <vaughn.devin.1@gmail.com>
2019-03-04 11:55:46 -0800

    override ranked gradebooks GetLetterGrade Function

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,47 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+            }
+
+            var threshold = Students.Count / 5;
+            var gradesHigherThanCurrent = 0;
+
+            foreach(var grade in Students)
+            {
+                if(averageGrade < grade.AverageGrade)
+                {
+                    gradesHigherThanCurrent++;
+                }
+            }
+
+            if(gradesHigherThanCurrent/threshold < 1)
+            {
+                return 'A';
+            }
+
+            else if (gradesHigherThanCurrent / threshold < 2)
+            {
+                return 'B';
+            }
+
+            else if (gradesHigherThanCurrent / threshold < 3)
+            {
+                return 'C';
+            }
+
+            else if (gradesHigherThanCurrent / threshold < 4)
+            {
+                return 'D';
+            }
+
+            else
+            return 'F';
+        }
     }
 }
